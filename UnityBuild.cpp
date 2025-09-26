59047453e100e6c9b426765f847c0c121a3f4457
Rohan Jadav <ro1.darkstar7@gmail.com>
2025-09-26 16:33:44 +0530

    updated minsdk version (in build bat) to 30 and updated unity build cpp to remove bs macro warnings

--- UnityBuild.cpp
+++ UnityBuild.cpp
@@ -1,14 +1,19 @@
+// doing these shenanigans to avoid some redefinition warnings
+#include "game-activity/Log.h"
 #include "game-activity/system_utils.cpp"
 #include "game-activity/jnictx.cpp"
+#undef LOG_TAG
 #include "game-activity/jni_helper.cpp"
 #include "game-activity/jni_wrap.cpp"
 #include "game-activity/apk_utils.cpp"
+#undef LOG_TAG
 #include "game-activity/gametextinput.cpp"
+#undef LOG_TAG
+#define LOG_TAG "GameActivity"
+#undef ALOGV
+#undef ALOGW
+#undef ALOGE
+#include "game-activity/GameActivityLog.h"
 #include "game-activity/GameActivityEvents.cpp"
 #include "game-activity/GameActivity.cpp"
-#include "NativeLib/app/src/main/cpp/AndroidOut.cpp"
-#include "NativeLib/app/src/main/cpp/main.cpp"
-#include "NativeLib/app/src/main/cpp/Renderer.cpp"
-#include "NativeLib/app/src/main/cpp/Shader.cpp"
-#include "NativeLib/app/src/main/cpp/TextureAsset.cpp"
-#include "NativeLib/app/src/main/cpp/Utility.cpp"
+#undef LOG_TAG
--- build.bat
+++ build.bat
@@ -43,9 +43,9 @@ if not exist .\Temp\arm64-v8a mkdir .\Temp\arm64-v8a
 
 echo.
 echo Building Android-x86
-%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c11 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=i686-none-linux-android28 ./UnityBuild.c -o ./Temp/x86/GameSDKC.o -c
-%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c++14 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=i686-none-linux-android28 ./UnityBuild.cpp -o ./Temp/x86/GameSDKCxx.o -c
-%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=i686-none-linux-android28 ./Temp/x86/GameSDKC.o ./Temp/x86/GameSDKCxx.o -o ./Temp/x86/GameSDKCxx.so -llog -landroid -shared -u Java_com_google_androidgamesdk_GameActivity_initializeNativeCode
+%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c11 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=i686-none-linux-android30 ./UnityBuild.c -o ./Temp/x86/GameSDKC.o -c
+%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c++14 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=i686-none-linux-android30 ./UnityBuild.cpp -o ./Temp/x86/GameSDKCxx.o -c
+%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=i686-none-linux-android30 ./Temp/x86/GameSDKC.o ./Temp/x86/GameSDKCxx.o -o ./Temp/x86/GameSDKCxx.so -llog -landroid -shared -u Java_com_google_androidgamesdk_GameActivity_initializeNativeCode
 if %ErrorLevel% equ 0 (
     echo Build succeeded.
 ) else (
@@ -54,9 +54,9 @@ if %ErrorLevel% equ 0 (
 
 echo.
 echo Building Android-x86_64
-%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c11 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=x86_64-none-linux-android28 ./UnityBuild.c -o ./Temp/x86_64/GameSDKC.o -c
-%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c++14 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=x86_64-none-linux-android28 ./UnityBuild.cpp -o ./Temp/x86_64/GameSDKCxx.o -c
-%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=x86_64-none-linux-android28 ./Temp/x86_64/GameSDKC.o ./Temp/x86_64/GameSDKCxx.o -o ./Temp/x86_64/GameSDKCxx.so -llog -landroid -shared -u Java_com_google_androidgamesdk_GameActivity_initializeNativeCode
+%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c11 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=x86_64-none-linux-android30 ./UnityBuild.c -o ./Temp/x86_64/GameSDKC.o -c
+%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c++14 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=x86_64-none-linux-android30 ./UnityBuild.cpp -o ./Temp/x86_64/GameSDKCxx.o -c
+%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=x86_64-none-linux-android30 ./Temp/x86_64/GameSDKC.o ./Temp/x86_64/GameSDKCxx.o -o ./Temp/x86_64/GameSDKCxx.so -llog -landroid -shared -u Java_com_google_androidgamesdk_GameActivity_initializeNativeCode
 if %ErrorLevel% equ 0 (
     echo Build succeeded.
 ) else (
@@ -65,9 +65,9 @@ if %ErrorLevel% equ 0 (
 
 echo.
 echo Building Android-armeabi-v7a
-%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c11 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=armv7a-none-linux-androideabi28 ./UnityBuild.c -o ./Temp/armeabi-v7a/GameSDKC.o -c
-%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c++14 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=armv7a-none-linux-androideabi28 ./UnityBuild.cpp -o ./Temp/armeabi-v7a/GameSDKCxx.o -c
-%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=armv7a-none-linux-androideabi28 ./Temp/armeabi-v7a/GameSDKC.o ./Temp/armeabi-v7a/GameSDKCxx.o -o ./Temp/armeabi-v7a/GameSDKCxx.so -llog -landroid -shared -u Java_com_google_androidgamesdk_GameActivity_initializeNativeCode
+%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c11 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=armv7a-none-linux-androideabi30 ./UnityBuild.c -o ./Temp/armeabi-v7a/GameSDKC.o -c
+%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c++14 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=armv7a-none-linux-androideabi30 ./UnityBuild.cpp -o ./Temp/armeabi-v7a/GameSDKCxx.o -c
+%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=armv7a-none-linux-androideabi30 ./Temp/armeabi-v7a/GameSDKC.o ./Temp/armeabi-v7a/GameSDKCxx.o -o ./Temp/armeabi-v7a/GameSDKCxx.so -llog -landroid -shared -u Java_com_google_androidgamesdk_GameActivity_initializeNativeCode
 if %ErrorLevel% equ 0 (
     echo Build succeeded.
 ) else (
@@ -76,9 +76,9 @@ if %ErrorLevel% equ 0 (
 
 echo.
 echo Building Android-arm64-v8a
-%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c11 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=aarch64-none-linux-android28 ./UnityBuild.c -o ./Temp/arm64-v8a/GameSDKC.o -c
-%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c++14 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=aarch64-none-linux-android28 ./UnityBuild.cpp -o ./Temp/arm64-v8a/GameSDKCxx.o -c
-%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=aarch64-none-linux-android28 ./Temp/arm64-v8a/GameSDKC.o ./Temp/arm64-v8a/GameSDKCxx.o -o ./Temp/arm64-v8a/GameSDKCxx.so -llog -landroid -shared -u Java_com_google_androidgamesdk_GameActivity_initializeNativeCode
+%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c11 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=aarch64-none-linux-android30 ./UnityBuild.c -o ./Temp/arm64-v8a/GameSDKC.o -c
+%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe -g -O0 -DDEBUG -DANDROID=1 -D_FORTIFY_SOURCE=2 -std=c++14 --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=aarch64-none-linux-android30 ./UnityBuild.cpp -o ./Temp/arm64-v8a/GameSDKCxx.o -c
+%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe --sysroot=%ANDROID_TOOLCHAIN%\toolchains\llvm\prebuilt\windows-x86_64\sysroot\ -fPIC --target=aarch64-none-linux-android30 ./Temp/arm64-v8a/GameSDKC.o ./Temp/arm64-v8a/GameSDKCxx.o -o ./Temp/arm64-v8a/GameSDKCxx.so -llog -landroid -shared -u Java_com_google_androidgamesdk_GameActivity_initializeNativeCode
 if %ErrorLevel% equ 0 (
     echo Build succeeded.
 ) else (
